14e3d2af7aff71ba2f569cc612ec3068c1200b32
Adirsflo <rida.abdal@gmail.com>
2023-12-07 18:43:01 +0100

    AddPlantWindow Completed!

--- GreenThumb/Windows/AddPlantWindow.xaml.cs
+++ GreenThumb/Windows/AddPlantWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using GreenThumb.Models;
+﻿using GreenThumb.Database;
+using GreenThumb.Models;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,6 +10,7 @@ namespace GreenThumb.Windows
 	/// </summary>
 	public partial class AddPlantWindow : Window
 	{
+		// Skapar en lista
 		public AddPlantWindow()
 		{
 			InitializeComponent();
@@ -50,37 +52,151 @@ namespace GreenThumb.Windows
 		}
 		private void btnAddInstruction_Click(object sender, RoutedEventArgs e) // Adding instructions to the new plant
 		{
-			InstructionModel newInstruciton = new()
+			if (!string.IsNullOrEmpty(txtNewInstructionName.Text.Trim()) && !string.IsNullOrEmpty(txtNewInstructionDescription.Text.Trim()))
 			{
-				Name = txtNewInstructionName.Text,
-				Description = txtNewInstructionDescription.Text,
-			};
+				InstructionModel newInstruciton = new()
+				{
+					Name = txtNewInstructionName.Text,
+					Description = txtNewInstructionDescription.Text,
+				};
 
-			ListViewItem item = new();
+				ListViewItem item = new();
 
-			item.Content = newInstruciton.Name;
-			item.Tag = newInstruciton;
-			lstAddInstruction.Items.Add(item);
+				item.Content = newInstruciton.Name;
+				item.Tag = newInstruciton;
+				lstAddInstruction.Items.Add(item);
 
-			txtNewInstructionName.Text = string.Empty;
-			txtNewInstructionDescription.Text = string.Empty;
+				txtNewInstructionName.Text = string.Empty;
+				txtNewInstructionDescription.Text = string.Empty;
 
-			MessageBox.Show("Instruction added");
+				MessageBox.Show("Instruction added");
+			}
+			else
+			{
+				MessageBox.Show("Please fill in the instruction fields", "Warning");
+			}
 		}
 		private void btnRemoveInstruction_Click(object sender, RoutedEventArgs e) // Removing instructions from the new plant
 		{
-			MessageBox.Show("Instruction removed successfully");
-		}
+			ListViewItem selectedItem = (ListViewItem)lstAddInstruction.SelectedItem;
 
+			if (selectedItem != null)
+			{
+				lstAddInstruction.Items.Remove(selectedItem);
+				MessageBox.Show("Instruction removed");
+			}
+			else if (lstAddInstruction.Items.Count != 0)
+			{
+				MessageBox.Show("Please select an instruction to remove", "Warning");
+			}
+			else
+			{
+				MessageBox.Show("There are no instructions to remove", "Warning");
+			}
+		}
 		private void btnAddPlant_Click(object sender, RoutedEventArgs e) // Adding plant to database
 		{
-			MessageBoxResult result = MessageBox.Show("Have you added all the instructions for this plant?", "Adding plant", MessageBoxButton.YesNo);
-			if (result == MessageBoxResult.Yes)
+			/*
+			 * 1. Kolla så inget är null
+			 * OM NÅGOT ÄR DET... skicka felmeddelande
+			 * 2. Samla information från Plant och lstAddInstruction
+			 * 3. Lägg till PlantId i InstructionModel
+			 * 3. Lägg till informationen i databasen
+			 * 4. Skicka meddelande till användaren
+			 */
+
+			PlantModel newPlant = new();
+			newPlant.Name = txtNewPlantName.Text.Trim();
+			newPlant.Type = txtNewPlantType.Text.Trim();
+			newPlant.Description = txtNewPlantDescription.Text.Trim();
+
+			try
 			{
-				MessageBox.Show("The plant has been added!", "Plant added");
-				PlantWindow plantWindow = new();
-				plantWindow.Show();
-				Close();
+				ValidateInputs();
+
+				if (IsPlantAvailable(txtNewPlantName.Text))
+				{
+					MessageBox.Show("Plant is already in use. Please add a different plant.");
+					return;
+				}
+
+
+				MessageBoxResult result = MessageBox.Show("Have you added all the instructions for this plant?", "Adding plant", MessageBoxButton.YesNo);
+				if (result == MessageBoxResult.Yes)
+				{
+					using (GreenThumbDbContext context = new())
+					{
+						GreenThumbRepository<PlantModel> plantRepository = new(context);
+						GreenThumbRepository<InstructionModel> instructionRepository = new(context);
+
+						plantRepository.Add(newPlant);
+						plantRepository.Complete();
+
+						foreach (ListViewItem instruction in lstAddInstruction.Items)
+						{
+							InstructionModel selectedInstruction = (InstructionModel)instruction.Tag;
+
+							//var instructionToAdd = (InstructionModel)instruction;
+							selectedInstruction.PlantId = newPlant.PlantId;
+
+							instructionRepository.Add(selectedInstruction);
+							instructionRepository.Complete();
+						}
+					}
+
+					MessageBox.Show("Plant successfully added! Thank you for contributing to a greener thumb!", "Plant added");
+					PlantWindow plantWindow = new();
+					plantWindow.Show();
+					Close();
+				}
+			}
+			catch (ArgumentException ex)
+			{
+				MessageBox.Show($"Error: {ex.Message}");
+			}
+			catch (Exception ex)
+			{
+				// Handle other exceptions that might occur
+				MessageBox.Show($"Unexpected error: {ex.Message}");
+			}
+		}
+		private void ValidateInputs()
+		{
+			ValidateNotEmptyPlant(txtNewPlantName.Text, "Plant name");
+			ValidateNotEmptyPlant(txtNewPlantType.Text, "Plant type");
+			ValidateNotEmptyPlant(txtNewPlantDescription.Text, "Plant description");
+			ValidateNotEmptyInstruction(); // Fixa
+		}
+		private void ValidateNotEmptyPlant(string input, string fieldName)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				throw new ArgumentException($"{fieldName} cannot be empty.");
+			}
+		}
+		private void ValidateNotEmptyInstruction()
+		{
+			if (lstAddInstruction.Items.Count == 0)
+			{
+				throw new ArgumentException("You must add atleast one instruction for the plant.");
+			}
+		}
+		private bool IsPlantAvailable(string plantCheck)
+		{
+			using (GreenThumbDbContext context = new())
+			{
+				GreenThumbRepository<PlantModel> plantRepository = new(context);
+
+				var gatherName = context.Plants.Where(p => p.Name == plantCheck).ToList();
+
+				if (gatherName.ToString() == plantCheck || gatherName.Count == 0)
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
 			}
 		}
 	}
--- GreenThumb/Windows/RegisterWindow.xaml.cs
+++ GreenThumb/Windows/RegisterWindow.xaml.cs
@@ -4,159 +4,159 @@ using System.Windows;
 
 namespace GreenThumb.Windows
 {
-    public partial class RegisterWindow : Window
-    {
-        public RegisterWindow()
-        {
-            InitializeComponent();
-        }
+	public partial class RegisterWindow : Window
+	{
+		public RegisterWindow()
+		{
+			InitializeComponent();
+		}
 
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
-            CancelToSignInWindow();
-        }
+		private void btnCancel_Click(object sender, RoutedEventArgs e)
+		{
+			CancelToSignInWindow();
+		}
 
-        private async void btnRegister_Click(object sender, RoutedEventArgs e) // Method for registering a new user
-        {
-            // 3. Check if username and Email is available
+		private async void btnRegister_Click(object sender, RoutedEventArgs e) // Method for registering a new user
+		{
+			// 3. Check if username and Email is available
 
-            // Declaring the variables
-            UserModel newUser = new();
-            newUser.Username = txtNewUsername.Text.Trim();
-            newUser.FirstName = txtNewFirstName.Text.Trim();
-            newUser.LastName = txtNewLastName.Text.Trim();
-            newUser.Email = txtNewEmail.Text.Trim();
-            newUser.Password = txtNewPassword.Password.Trim();
+			// Declaring the variables
+			UserModel newUser = new();
+			newUser.Username = txtNewUsername.Text.Trim();
+			newUser.FirstName = txtNewFirstName.Text.Trim();
+			newUser.LastName = txtNewLastName.Text.Trim();
+			newUser.Email = txtNewEmail.Text.Trim();
+			newUser.Password = txtNewPassword.Password.Trim();
 
-            // Handeling exceptions: catches the error if its null, empty or doesnt contain "@"
-            try
-            {
-                ValidateInputsAsync();
+			// Handeling exceptions: catches the error if its null, empty or doesnt contain "@"
+			try
+			{
+				ValidateInputsAsync();
 
-                if (await IsUsernameInUseAsync(txtNewUsername.Text, txtNewEmail.Text))
-                {
-                    MessageBox.Show("Username or email is already in use. Please choose a different username or email.");
-                    return;
-                }
+				if (await IsUsernameInUseAsync(txtNewUsername.Text, txtNewEmail.Text))
+				{
+					MessageBox.Show("Username or email is already in use. Please choose a different username or email.");
+					return;
+				}
 
-                using (GreenThumbDbContext context = new())
-                {
-                    GreenThumbRepository<UserModel> userRepository = new(context);
+				using (GreenThumbDbContext context = new())
+				{
+					GreenThumbRepository<UserModel> userRepository = new(context);
 
-                    userRepository.Add(newUser);
-                    userRepository.Complete();
-                    // Kalla på repository (AKA new:a upp i denna rutan)
-                    // Lägga till i databasen
-                    // Spara ändringar
-                }
+					userRepository.Add(newUser);
+					userRepository.Complete();
+					// Kalla på repository (AKA new:a upp i denna rutan)
+					// Lägga till i databasen
+					// Spara ändringar
+				}
 
-                //// TODO: Remove Asynchronic programming
-                //await Task.Run(() =>
-                //{
-                //    using (GreenThumbDbContext context = new())
-                //    {
-                //        GreenThumbRepository<UserModel> greenThumbRepository = new(context);
-                //        // Delaya för att se om async fungerar
-                //        greenThumbRepository.Add(newUser);
-                //        greenThumbRepository.Complete();
+				//// TODO: Remove Asynchronic programming
+				//await Task.Run(() =>
+				//{
+				//    using (GreenThumbDbContext context = new())
+				//    {
+				//        GreenThumbRepository<UserModel> greenThumbRepository = new(context);
+				//        // Delaya för att se om async fungerar
+				//        greenThumbRepository.Add(newUser);
+				//        greenThumbRepository.Complete();
 
-                //    }
-                //});
+				//    }
+				//});
 
-                MessageBox.Show("Account successfully created! Welcome to a greener life!", "Account created");
+				MessageBox.Show("Account successfully created! Welcome to a greener life!", "Account created");
 
-                SignInWindow signInWindow = new();
-                signInWindow.Show();
-                Close();
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show($"Error: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                // Handle other exceptions that might occur
-                MessageBox.Show($"Unexpected error: {ex.Message}");
-            }
-        }
-        private void ValidateInputsAsync()
-        {
-            ValidateNotEmptyAsync(txtNewUsername.Text, "Username");
-            ValidateNotEmptyAsync(txtNewFirstName.Text, "First name");
-            ValidateNotEmptyAsync(txtNewLastName.Text, "Last name");
-            ValidateNotEmptyAsync(txtNewEmail.Text, "Email");
-            ValidateEmailFormatAsync(txtNewEmail.Text);
-            ValidatePasswordAsync(txtNewPassword.Password, txtConfirmPassword.Password);
-        }
-        private void ValidateNotEmptyAsync(string input, string fieldName)
-        {
-            if (string.IsNullOrEmpty(input))
-            {
-                throw new ArgumentException($"{fieldName} cannot be empty.");
-            }
-        }
-        private void ValidateEmailFormatAsync(string email)
-        {
-            if (!email.Contains("@"))
-            {
-                throw new ArgumentException("Invalid input for email. The email must contain the \"@\" symbol.");
-            }
-        }
-        private void ValidatePasswordAsync(string newPassword, string confirmPassword)
-        {
-            if (string.IsNullOrEmpty(newPassword) && string.IsNullOrEmpty(confirmPassword))
-            {
-                throw new ArgumentException("Please enter a new password.");
-            }
-            else if (newPassword != confirmPassword)
-            {
-                throw new ArgumentException("The password doesn't match. Please try again.");
-            }
-        }
-        private async Task<bool> IsUsernameInUseAsync(string usernameCheck, string emailCheck)
-        {
-            using (GreenThumbDbContext context = new())
-            {
-                UserRepository userRepository = new(context);
+				SignInWindow signInWindow = new();
+				signInWindow.Show();
+				Close();
+			}
+			catch (ArgumentException ex)
+			{
+				MessageBox.Show($"Error: {ex.Message}");
+			}
+			catch (Exception ex)
+			{
+				// Handle other exceptions that might occur
+				MessageBox.Show($"Unexpected error: {ex.Message}");
+			}
+		}
+		private void ValidateInputsAsync()
+		{
+			ValidateNotEmptyAsync(txtNewUsername.Text, "Username");
+			ValidateNotEmptyAsync(txtNewFirstName.Text, "First name");
+			ValidateNotEmptyAsync(txtNewLastName.Text, "Last name");
+			ValidateNotEmptyAsync(txtNewEmail.Text, "Email");
+			ValidateEmailFormatAsync(txtNewEmail.Text);
+			ValidatePasswordAsync(txtNewPassword.Password, txtConfirmPassword.Password);
+		}
+		private void ValidateNotEmptyAsync(string input, string fieldName)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				throw new ArgumentException($"{fieldName} cannot be empty.");
+			}
+		}
+		private void ValidateEmailFormatAsync(string email)
+		{
+			if (!email.Contains("@"))
+			{
+				throw new ArgumentException("Invalid input for email. The email must contain the \"@\" symbol.");
+			}
+		}
+		private void ValidatePasswordAsync(string newPassword, string confirmPassword)
+		{
+			if (string.IsNullOrEmpty(newPassword) && string.IsNullOrEmpty(confirmPassword))
+			{
+				throw new ArgumentException("Please enter a new password.");
+			}
+			else if (newPassword != confirmPassword)
+			{
+				throw new ArgumentException("The password doesn't match. Please try again.");
+			}
+		}
+		private async Task<bool> IsUsernameInUseAsync(string usernameCheck, string emailCheck)
+		{
+			using (GreenThumbDbContext context = new())
+			{
+				UserRepository userRepository = new(context);
 
-                if (await userRepository.GetByUsername(usernameCheck) == null)
-                {
-                    return false;
-                }
-                else if (await userRepository.GetByEmail(emailCheck) == null)
-                {
-                    return false;
-                }
+				if (await userRepository.GetByUsername(usernameCheck) == null)
+				{
+					return false;
+				}
+				else if (await userRepository.GetByEmail(emailCheck) == null)
+				{
+					return false;
+				}
 
-                return true;
-            }
-        }
+				return true;
+			}
+		}
 
-        private void CancelToSignInWindow() // Method for returning to SignInWindow
-        {
-            if (txtNewUsername.Text != ""
-                || txtNewFirstName.Text != ""
-                || txtNewLastName.Text != ""
-                || txtNewPassword.Password != ""
-                || txtConfirmPassword.Password != ""
-                || txtNewEmail.Text != "")
-            {
-                MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel?" +
-                    "\n\nYour progress will not be saved!",
-                    "Warning", MessageBoxButton.YesNo);
-                if (result == MessageBoxResult.Yes)
-                {
-                    SignInWindow signInWindow = new();
-                    signInWindow.Show();
-                    Close();
-                }
-            }
-            else
-            {
-                SignInWindow signInWindow = new();
-                signInWindow.Show();
-                Close();
-            }
-        }
-    }
+		private void CancelToSignInWindow() // Method for returning to SignInWindow
+		{
+			if (txtNewUsername.Text != ""
+					|| txtNewFirstName.Text != ""
+					|| txtNewLastName.Text != ""
+					|| txtNewPassword.Password != ""
+					|| txtConfirmPassword.Password != ""
+					|| txtNewEmail.Text != "")
+			{
+				MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel?" +
+						"\n\nYour progress will not be saved!",
+						"Warning", MessageBoxButton.YesNo);
+				if (result == MessageBoxResult.Yes)
+				{
+					SignInWindow signInWindow = new();
+					signInWindow.Show();
+					Close();
+				}
+			}
+			else
+			{
+				SignInWindow signInWindow = new();
+				signInWindow.Show();
+				Close();
+			}
+		}
+	}
 }
